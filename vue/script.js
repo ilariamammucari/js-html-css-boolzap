4844273819a31ce6d3e85d5144a9e364a0b820cd
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-02 10:29:07 +0100

    prova

---
+++ vue/script.js
@@ -0,0 +1,105 @@
+var app = Vue({
+    el: '#root',
+    data: {
+        users: [
+            {
+                nome: 'Michele',
+                avatar: 'img/avatar_1.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Fabio',
+                avatar: 'img/avatar_2.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao,come stai?',
+                        stato: 'ricevuto'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Samuele',
+                avatar: 'img/avatar_3.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Antonio',
+                avatar: 'img/avatar_4.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Tommaso',
+                avatar: 'img/avatar_5.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'ricevuto'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Viviana',
+                avatar: 'img/avatar_6.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Luca',
+                avatar: 'img/avatar_7.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Matteo',
+                avatar: 'img/avatar_8.jpg',
+                messaggio: [
+                    {
+                        data: '02/02/21 alle 09:40',
+                        testo: 'Ciao',
+                        stato: 'inviato'
+                    }
+                ]
+            }
+        ]
+    }
+
+
+    
+});
